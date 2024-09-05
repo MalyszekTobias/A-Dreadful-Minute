ca6ad9a0d093a7b365014783f75acfc4a459a0d4
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2024-09-05 21:14:39 +0200

    verion 0.2.1:
    + added point at to the debug overlay (needs some tweaking)
    
    = renamed text_rect to rect in custom_text.py

--- app/custom_text.py
+++ app/custom_text.py
@@ -19,18 +19,18 @@ class Custom_text:  # A class that generates text
         self.font = pygame.font.Font(font, self.font_height)
 
         self.text_to_render = self.font.render(self.text, True, self.text_color, self.background_color)
-        self.text_rect = self.text_to_render.get_rect()
+        self.rect = self.text_to_render.get_rect()
 
         if self.center:  # If self.center == True it sets the center of the text as self.x and self.y
-            self.text_rect.center = (self.x, self.y)
+            self.rect.center = (self.x, self.y)
         else:  # Else it set self.x and self.y as the top left corner of the text
-            self.text_rect.center = (self.x + self.text_rect.width//2, self.y + self.text_rect.height//2)
+            self.rect.center = (self.x + self.rect.width//2, self.y + self.rect.height//2)
 
         self.display.objects.append(self)
 
     def render(self):  # Renders the text
         if not self.hidden:
-            self.display.screen.blit(self.text_to_render, self.text_rect)
+            self.display.screen.blit(self.text_to_render, self.rect)
 
     def events(self, event):  # For now just passes when checking events
         pass
@@ -42,9 +42,9 @@ class Custom_text:  # A class that generates text
     def update_text(self, text):
         self.text = text
         self.text_to_render = self.font.render(self.text, True, self.text_color, self.background_color)
-        self.text_rect = self.text_to_render.get_rect()
+        self.rect = self.text_to_render.get_rect()
 
         if self.center:  # If self.center == True it sets the center of the text as self.x and self.y
-            self.text_rect.center = (self.x, self.y)
+            self.rect.center = (self.x, self.y)
         else:  # Else it set self.x and self.y as the top left corner of the text
-            self.text_rect.center = (self.x + self.text_rect.width // 2, self.y + self.text_rect.height // 2)
+            self.rect.center = (self.x + self.rect.width // 2, self.y + self.rect.height // 2)
--- app/game.py
+++ app/game.py
@@ -29,13 +29,16 @@ class Game:
         self.displays = {'template_display': display.basic_display(self), 'start_screen': display.start_screen(self), 'settings_screen': display.settings_screen(self)}
         self.current_display = self.displays['start_screen']
 
+        self.pointing_at = []
+
         self.debug = False
         self.debug_items = [custom_text.Custom_text(self, 12, 15, self.font, 30, f'Current version: {self.version}', text_color='white', center=False),
                             custom_text.Custom_text(self, 12, 45, self.font, 30, f'Resolution: {self.width}x{self.height}', text_color='white', center=False),
                             custom_text.Custom_text(self, 12, 75, self.font, 30, f'FPS cap: {self.fps}', text_color='white', center=False),
                             custom_text.Custom_text(self, 12, 105, self.font, 30, f'FPS: {self.clock.get_fps()}', text_color='white', center=False),
                             custom_text.Custom_text(self, 12, 135, self.font, 30, f'Objects in memory: {len(self.current_display.objects)}', text_color='white', center=False),
-                            custom_text.Custom_text(self, 12, 165, self.font, 30, f'Current display: {type(self.current_display)}', text_color='white', center=False)]
+                            custom_text.Custom_text(self, 12, 165, self.font, 30, f'Current display: {type(self.current_display)}', text_color='white', center=False),
+                            custom_text.Custom_text(self, 12, 195, self.font, 30, f'Pointing at: {self.pointing_at}', text_color='white', center=False)]
 
 
         for debug_item in self.debug_items:
@@ -70,8 +73,26 @@ class Game:
 
     def update(self):
         if self.debug:
+
+            for obj in self.current_display.objects:
+                if obj.rect.collidepoint(pygame.mouse.get_pos()):
+                    if obj not in self.pointing_at:
+                        self.pointing_at.append(obj)
+
+            i = []
+            for obj in self.pointing_at:
+                if obj.rect.collidepoint(pygame.mouse.get_pos()) == False:
+                    i.append(obj)
+            for obj in i:
+                self.pointing_at.remove(obj)
+            i = []
+
+
             self.debug_items[3].update_text(f'FPS: {self.clock.get_fps()}')
             self.debug_items[4].update_text(f'Objects in memory: {len(self.current_display.objects)}')
             self.debug_items[5].update_text(f'Current display: {type(self.current_display)}')
+            self.debug_items[6].update_text(f'Pointing at: {self.pointing_at}')
+
+
         pygame.display.update()
         pygame.display.flip()
