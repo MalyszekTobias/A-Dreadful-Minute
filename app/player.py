5fbbe0d93b3ec003a9649b25a3e9f77bccc62e06
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2024-09-08 13:21:55 +0200

    added player 2

--- app/player.py
+++ app/player.py
@@ -4,11 +4,24 @@ import pygame.draw
 class player:
     def __init__(self, display):
         self.display = display
+        self.x = 100
+        self.y = 100
+
+        self.rect = pygame.Rect(self.x, self.y, 100, 100)
 
         self.display.objects.append(self)
 
     def render(self):
-        pygame.draw.rect(self.display.screen, (255, 255, 255), (50, 50, 100, 100))
+        pygame.draw.rect(self.display.screen, (255, 0, 255), self.rect)
 
     def events(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                self.x -= 5
+
+            self.update_rect()
+
+    def update_rect(self):
+        self.rect = pygame.Rect(self.x, self.y, 100, 100)
+
+
